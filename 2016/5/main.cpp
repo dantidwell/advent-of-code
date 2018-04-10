309817fc0be0a0a24346ef1ab116dccbd41f76b5
dantidwell <dantidwell921@gmail.com>
2018-04-10 16:57:03 -0500

    remove extraneous var definitions

--- 2016/5/main.cpp
+++ 2016/5/main.cpp
@@ -237,13 +237,10 @@ void Init(const char * seed, uint32_t  Input[][8]) {
   }
 }
 
-MEMALIGN(64) uint32_t Input[16][8] = { 0 };
-MEMALIGN(64) uint32_t Hashes[8] = { 0 };
-uint8_t Chars[16] = { '0','1','2','3','4','5','6','7','8','9','a', 'b', 'c', 'd', 'e', 'f'};
-
 int main() {  
   MEMALIGN(64) uint32_t Input[16][8];
   MEMALIGN(64) uint32_t Hashes[8] = { 0 };
+  uint8_t Chars[16] = { '0','1','2','3','4','5','6','7','8','9','a', 'b', 'c', 'd', 'e', 'f'};
   
   /* Part One */
   uint8_t passwordOne[9] = { 0 };
